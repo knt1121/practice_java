4c79816ee22b1876dcd2d2a1240190e309ca566f
knt1121 <knt1121@outlook.jp>
2024-04-16 22:53:10 +0900

    edit

--- Chapter2.java
+++ Chapter2.java
@@ -1,5 +1,12 @@
 public class Chapter2 {
     public static void main(String[] args) {
-        System.out.println("Hello.java");
+        //1次元配列の宣言方法は以下2行
+        //データ型[] 配列名;
+        //配列名 = new データ型 [要素数];
+        int[] student;
+        student = new int[40];
+        //配列の初期化（領域の確保と値（初期値）の代入までを行う）
+        //データ型[] 配列名 = {初期値1、初期値2、初期値3、......}
+        String[] size = {S,M,L}
     }
 }
